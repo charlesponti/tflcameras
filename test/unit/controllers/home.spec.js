cb7a14e9ca31b29fab7714ea531530ca2e28e387
CJ Ponti <cjponti@gmail.com>
2015-01-04 12:06:42 +0000

    Fix home controller test

--- test/unit/controllers/home.spec.js
+++ test/unit/controllers/home.spec.js
@@ -1,23 +1,35 @@
 describe('Controllers: HomeCtrl', function() {
   'use strict';
 
-  var ctrl;
+  var ctrl, cameraService;
 
   beforeEach(function() {
+    // Load application
     angular.mock.module('app');
 
-    angular.mock.inject(function($controller) {
-      ctrl = $controller('HomeCtrl');
+    // Inject test dependecies
+    angular.mock.inject(function($controller, $rootScope, Cameras, $q) {
+      spyOn(Cameras, 'get').and.returnValue($q.defer().promise);
+      cameraService = Cameras;
+      ctrl = $controller('HomeCtrl', {
+        $scope: $rootScope,
+        Cameras: cameraService
+      });
     });
   });
 
   afterEach(function() {
-    ctrl = undefined;
+    ctrl =
+    cameraService = undefined;
+  });
+
+  it('should load cameras', function() {
+    expect(cameraService.get).toHaveBeenCalled();
   });
 
   describe('.title', function() {
     it('should have correct value', function() {
-      expect(ctrl.title).toEqual("Welcome to Facade-Angular");
+      expect(ctrl.title).toEqual('The TFL Is Watching');
     });
   });
 
