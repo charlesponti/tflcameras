dff86a23a4a4df5bf860f625bc8e017dd520e1c6
CJ Ponti <cjponti@gmail.com>
2015-01-04 14:07:37 +0000

    Add tests for home controller

--- test/unit/controllers/home.spec.js
+++ test/unit/controllers/home.spec.js
@@ -33,4 +33,37 @@ describe('Controllers: HomeCtrl', function() {
     });
   });
 
+  describe('.closeInfos()', function() {
+    it('should call .close() on vm.infos', function() {
+      var infowindow = {
+        close: jasmine.createSpy('close')
+      };
+      ctrl.infos = [
+        infowindow
+      ];
+      ctrl.closeInfos();
+      expect(infowindow.close).toHaveBeenCalled();
+    });
+  });
+
+  describe('.onMarkerClick()', function() {
+    it('should close info windows and open new infowindow', function() {
+      var infowindow = {
+        open: jasmine.createSpy('open'),
+        close: jasmine.createSpy('close')
+      };
+      spyOn(ctrl, 'closeInfos');
+      ctrl.onMarkerClick('fooMap', 'fooMarker', infowindow);
+      expect(ctrl.closeInfos).toHaveBeenCalled();
+      expect(infowindow.open).toHaveBeenCalledWith('fooMap', 'fooMarker');
+    });
+  });
+
+  describe('.onCameraLoadSuccess()', function() {
+    it('should add cameras from response to ctrl.cameras', function() {
+      ctrl.onCameraLoadSuccess({ cameras: 'foo' });
+      expect(ctrl.cameras).toEqual('foo');
+    });
+  });
+  
 });
