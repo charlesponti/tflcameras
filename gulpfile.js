1690577bc1f99f7b2176007f3b2ed2ab4ef006c5
🇵🇷 <cj@ponti.io>
2017-07-06 15:05:51 +0100

    fix(gulp.cameras): Fix task for retrieving cameras

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
-'use strict';
+'use strict'
 
 // Development versions are default
-global.isProd = false;
+global.isProd = false
 
 // Current working directory
-var cwd = process.cwd();
+let cwd = process.cwd()
 
-var runSequence = require('run-sequence');
+let runSequence = require('run-sequence')
+const axios = require('axios')
 
 // Gulp & Plugins
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+let gulp = require('gulp')
+let $ = require('gulp-load-plugins')()
 
-var gulpif = $.if;
-var concat = $.concat;
-var uglify = $.uglify;
-var streamify = $.streamify;
-var minifyHTML = $.minifyHtml;
-var templateCache = $.angularTemplatecache;
+let gulpif = $.if
+let uglify = $.uglify
+let streamify = $.streamify
+let minifyHTML = $.minifyHtml
+let templateCache = $.angularTemplatecache
 
 // Styles
-var sass = require('gulp-sass');
-var minifycss = require('gulp-minify-css');
+let sass = require('gulp-sass')
+let minifycss = require('gulp-minify-css')
 
 // Scripts
-var jshint = require('gulp-jshint');
-var browserify = require('browserify');
-var stylish = require('jshint-stylish');
-var source = require('vinyl-source-stream');
-var ngannotate = require('browserify-ngannotate');
+let jshint = require('gulp-jshint')
+let browserify = require('browserify')
+let stylish = require('jshint-stylish')
+let source = require('vinyl-source-stream')
+let ngannotate = require('browserify-ngannotate')
 
 // Testing
-var karma = require('karma');
-var protractor = require('gulp-protractor').protractor;
-var webdriver = require('gulp-protractor').webdriver;
-var webdriverUpdate = require('gulp-protractor').webdriver_update;
+let karma = require('karma')
+let protractor = require('gulp-protractor').protractor
+let webdriver = require('gulp-protractor').webdriver
+let webdriverUpdate = require('gulp-protractor').webdriver_update
 
 // Dev Server
-var superstatic = require('superstatic');
+const serve = require('serve')
 
-gulp.task('webdriver-update', webdriverUpdate);
-gulp.task('webdriver', webdriver);
+gulp.task('webdriver-update', webdriverUpdate)
+gulp.task('webdriver', webdriver)
 
-var files = {
+let files = {
   scripts: {
     main: './src/scripts/main.js',
     source: 'src/scripts/**/*.js',
@@ -57,28 +57,28 @@ var files = {
     source: 'src/**/*.html',
     build: 'build/'
   }
-};
+}
 
-gulp.task('jshint', function() {
+gulp.task('jshint', function () {
   return gulp.src(files.scripts.source)
     .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-});
+    .pipe(jshint.reporter(stylish))
+})
 
-gulp.task('scripts', ['jshint'], function() {
+gulp.task('scripts', ['jshint'], function () {
   return browserify({
-      entries: [files.scripts.main],
-      debug: global.isProd ? false : true,
-      insertGlobals: true,
-      transform: ngannotate
-    })
+    entries: [files.scripts.main],
+    debug: !global.isProd,
+    insertGlobals: true,
+    transform: ngannotate
+  })
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulpif(global.isProd, streamify(uglify())))
-    .pipe(gulp.dest(files.scripts.build));
-});
+    .pipe(gulp.dest(files.scripts.build))
+})
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src(files.styles.main)
     .pipe(sass({
       sourceComments: global.isProd ? 'none' : 'map',
@@ -86,90 +86,83 @@ gulp.task('styles', function() {
       outputStyle: global.isProd ? 'compressed' : 'nested'
     }))
     .pipe(gulpif(global.isProd, minifycss()))
-    .pipe(gulp.dest(files.styles.build));
-});
+    .pipe(gulp.dest(files.styles.build))
+})
 
-gulp.task('views', function() {
+gulp.task('views', function () {
   gulp.src('src/index.html')
-      .pipe(minifyHTML({
-        comments: global.isProd ? false : true,
-        spare: global.isProd ? false : true,
-        empty: true
-      }))
-      .pipe(gulp.dest(files.html.build));
+    .pipe(minifyHTML({
+      comments: !global.isProd,
+      spare: !global.isProd,
+      empty: true
+    }))
+    .pipe(gulp.dest(files.html.build))
 
   return gulp.src('./src/views/**/*.html')
     .pipe(templateCache({
       standalone: true
     }))
-    .pipe(gulp.dest('./src/scripts'));
-});
+    .pipe(gulp.dest('./src/scripts'))
+})
 
-gulp.task('test', function(done) {
+gulp.task('test', function (done) {
   return karma.server.start({
     configFile: cwd + '/karma.conf.js'
-  }, done);
-});
+  }, done)
+})
 
-gulp.task('protractor', ['webdriver-update', 'webdriver' ], function() {
+gulp.task('protractor', ['webdriver-update', 'webdriver'], function () {
   return gulp.src('test/e2e/**/*.js')
     .pipe(protractor({
-        configFile: './protractor.conf.js',
+      configFile: './protractor.conf.js'
     }))
-    .on('error', function(err) {
+    .on('error', function (err) {
       // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
-});
+      throw err
+    })
+})
 
-gulp.task('watch', function() {
-  gulp.watch(files.html.source, ['views']);
-  gulp.watch(files.scripts.source, ['scripts']);
-  return gulp.watch(files.styles.source, ['styles']);
-});
+gulp.task('watch', function () {
+  gulp.watch(files.html.source, ['views'])
+  gulp.watch(files.scripts.source, ['scripts'])
+  return gulp.watch(files.styles.source, ['styles'])
+})
 
-gulp.task('server', function(next) {
-  var server = superstatic({
-    logger: {
-      info: function(msg) {
-        console.log('Info:', msg);
-      },
-      error: function(msg) {
-        console.error('Error:', msg);
+gulp.task('server', function (next) {
+  serve({port: 3000})
+
+  return next()
+})
+
+gulp.task('cameras', function (next) {
+  let fs = require('fs')
+  let parser = require('xml2json')
+
+  axios
+    .get('https://www.tfl.gov.uk/cdn/static/cms/documents/camera-list.xml')
+    .then(function (response) {
+      // let camerasXML = fs.readFileSync('./tflcameras.xml')
+      let camerasJSON = JSON.parse(parser.toJson(response.data))
+      let cameras = {
+        cameras: camerasJSON.syndicatedFeed.cameraList.camera.map(function (c) {
+          return Object.assign({}, c, { file: '0000' + c.id + '.jpg' })
+        })
       }
-    },
-    port: 3000,
-    config: 'divshot.json'
-  });
-  server.listen(function() {
-    console.log('Server running on port ' + server.port);
-  });
-  return next();
-});
+      fs.writeFileSync('./build/cameras.json', JSON.stringify(cameras))
+    })
 
-gulp.task('cameras', function(next) {
-  var fs = require('fs');
-  var parser = require('xml2json');
-  // Cameras from:
-  // https://www.tfl.gov.uk/cdn/static/cms/documents/camera-list.xml
-  var camerasXML = fs.readFileSync('./tflcameras.xml');
-  var camerasJSON = JSON.parse(parser.toJson(camerasXML));
-  var cameras = {
-    cameras: camerasJSON.syndicatedFeed.cameraList.camera
-  };
-  fs.writeFileSync('./build/cameras.json', JSON.stringify(cameras));
-  return next();
-});
+  return next()
+})
 
-gulp.task('prod', function() {
-  global.isProd = true;
-  return runSequence('views', 'styles', 'scripts');
-});
+gulp.task('prod', function () {
+  global.isProd = true
+  return runSequence('views', 'styles', 'scripts')
+})
 
-gulp.task('build', function() {
-  return runSequence('views', 'styles', 'scripts');
-});
+gulp.task('build', function () {
+  return runSequence('views', 'styles', 'scripts')
+})
 
-gulp.task('default', function() {
-  return runSequence('build', 'watch', 'server');
-});
+gulp.task('default', function () {
+  return runSequence('build', 'watch', 'server')
+})
